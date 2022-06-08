70cf2bd20754e16f21392136cfe7764d321b748d
selvaraja-nr <nr.selvaraja@gmail.com>
2022-06-08 14:19:25 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -9,19 +9,16 @@ export default function App() {
 
   useEffect(() => {
     async function getUsers() {
-      const response = await axios.get(apiURL);
+      await axios
+        .get(apiURL)
 
-      try {
-        setlistUsers(response.data);
-        // console.log(response.data);
-      } catch (error) {
-        // if (reponse.error) {
-        console.log('Error Occured');
-        console.log(error.response.data);
-        console.log(error.response.status);
-        console.log(error.response.headers);
-        // }
-      }
+        .then((response) => {
+          setlistUsers(response.data);
+        })
+        .catch((error) => {
+          console.log('Error Occured');
+          console.log(error);
+        });
     }
     getUsers();
   }, []);
